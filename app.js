67ca57c7a02e4c4a88fc377a00563bd77123b8cd
Ivan Korneychuk <ikorneyc@uwaterloo.ca>
2024-01-24 19:44:22 -0500

    Created db using MongoDB, linked it to server using Mongoose

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
 const port = 3000;
 
 app.get("/hello", (req, res) => {
@@ -11,6 +14,15 @@ app.use(express.json());
 
 app.use("/api/v1/tasks", tasks);
 
-app.listen(port, () => {
-  console.log(`The server is listening on port ${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`The server is listening on port ${port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
---
+++ models/tatsks.js
@@ -0,0 +1,4 @@
+const mongoose = require("mongoose");
+
+//This sets the structure of the task manager (only strings and bools)
+const taskSchema = new mongoose.Schema();
